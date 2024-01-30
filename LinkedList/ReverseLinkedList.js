894667137a872746bc54fcd32b4b1f5db91cdb37
Deep <dprajap2@gmail.com>
2024-01-30 12:17:14 -0500

    add solution for reverse linked list

---
+++ LinkedList/ReverseLinkedList.js
@@ -0,0 +1,29 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Approach: We can use three pointers to keep track of the current, previous, and next nodes. We can initialize the current node as the head and the previous and next nodes as null.
+// We can then loop through the array and move the pointers forward until the current node is null.
+
+const reverseList = (head) => {
+  let currhead = head;
+
+  let currentNode = currhead;
+  let prevNode = null;
+  let nextNode = null;
+
+  while (currentNode) {
+    //This sets the next node to the currents next node and then points the current node's next to the prevNode. This swaps the direction of of arrow from -> to <-.
+    nextNode = currentNode.next;
+    currentNode.next = prevNode;
+    // Advance the prev and current node.
+
+    prevNode = currentNode;
+    currentNode = nextNode;
+    nextNode = null;
+  }
+  // set new head of the list
+  currhead = prevNode;
+  return currhead;
+};
+
+//BigO Time Complexity: O(n) -> one for loop
+//BigO Space Complexity: O(1) -> storing the values in variables
