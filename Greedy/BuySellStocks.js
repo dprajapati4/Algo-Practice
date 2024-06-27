41424319da80ec75c71a37f4562ab8e96af47f1a
Deep <dprajap2@gmail.com>
2024-06-27 12:48:09 -0400

    add bf approach to buy and sell stock

--- Greedy/BuySellStocks.js
+++ Greedy/BuySellStocks.js
@@ -5,11 +5,7 @@
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 // Approach: Two Pointers
-// We can use two pointers to keep track of the of the buy day and the sell day. We can initialize the buy day as the first day and the sell day as the second day.
-// We can then loop through the array and move the pointers forward until the sellDay has reached the last item in the array.
-// If the buyDay is less than the sellDay value we can check if the current profit is greater than the max profit. If it is we can set the max profit to the current profit.
-// Otherwise we can move the buyDay pointer up to the sellDay pointer. Regardless to end this turn we move the sellDay pointer up.
-// In the end we return the max profit.
+// We can use two pointers to keep track of the of the buy day and the sell day. We can initialize the buy day as the first day and the sell day as the second day. We can then loop through the array and move the pointers forward until the sellDay has reached the last item in the array. If the buyDay is less than the sellDay value we can check if the current profit is greater than the max profit. If it is we can set the max profit to the current profit. Otherwise we can move the buyDay pointer up to the sellDay pointer. Regardless to end this turn we move the sellDay pointer up. In the end we return the max profit.
 
 const maxProfit = (prices) => {
   let buyDay = 0;
@@ -30,6 +26,27 @@ const maxProfit = (prices) => {
 
   return maxProfit;
 };
+// BigO
+// Time Complexity: O(n) -> one for loop
+// BigO Space Complexity: O(1) -> storing the values in variables
 
-//BigO Time Complexity: O(n) -> one for loop
-//BigO Space Complexity: O(1) -> storing the values in variables
+// Brute Force Approach: Use a nested for loop, track the profit and maxProfit, update maxProfit as needed.
+
+const maxProfitBF = function (prices) {
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      let profit = prices[j] - prices[i];
+
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+
+  return maxProfit;
+};
+// BigO
+// Time Complexity: O(n^2) -> use a nested for loop.
+// Space Complexity: O(1) -> storing the values in variables
