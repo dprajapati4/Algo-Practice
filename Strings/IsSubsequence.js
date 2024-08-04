01de3d5b814a2457c89441e1755301f955bd5f39
Deep <dprajap2@gmail.com>
2024-08-04 15:14:57 -0400

    add alt solution to isSubsequence

--- Strings/IsSubsequence.js
+++ Strings/IsSubsequence.js
@@ -27,3 +27,31 @@ const isSubsequence = (s, t) => {
 // BigO
 // Time Complexity: O(n) because we go through all the elements in the height array.
 // Space Complexity: O(1) because we are only creating new constants.
+
+// Alternative approach: We iterate over t, and keep track of s's index. If we match values, we increment s's index, if the index we are tracking equals the length of s and we found that value in t, it means we found all values of s and we can end the loop and return true. After iterating over t, we default to returning false, meaning not all letter in s were found in t.
+
+const isSubsequenceAlt = (s, t) => {
+  // EC: If s is an empty string than its a substring and we can return true.
+  if (s === "") return true;
+
+  // EC: If s is larger than t, it cant be a subsequence.
+  if (s.length > t.length) return false;
+
+  let curr = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === s[curr]) {
+      if (curr === s.length - 1) {
+        return true;
+      } else {
+        curr++;
+      }
+    }
+  }
+
+  return false;
+};
+
+// BigO
+// Time Complexity: O(n) because we go through all the elements in the height array.
+// Space Complexity: O(1) because we are only creating new constants.
